9c65aaa78db8c12b6a2dc2387e071241b73d408f
Bartek Zagórski <zagora171991@gmail.com>
2020-04-21 17:02:57 +0200

    add class XmlFile

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include "User.h"
+#include "XmlFile.h"
 
 using namespace std;
 
 int main()
 {
-    User user;
-    user.setUserId(3);
-    cout<<user.getUserId();
     return 0;
 }
